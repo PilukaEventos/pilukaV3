322bab1d09f38e64919d726056d7d4ed04e87186
PilukaEventos <155663391+PilukaEventos@users.noreply.github.com>
2024-10-15 04:13:21 +0100

    tudo bem

---
+++ database/migrations/2024_10_15_022027_create_fotos_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('fotos', function (Blueprint $table) {
+            $table->id();
+            $table->foto();
+            $table->espaco_id();
+            $table->parceiro_id();
+            $table->user_id();
+            $table->cliente_id();
+            $table->comentario_id();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('fotos');
+    }
+};
